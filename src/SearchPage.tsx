532bb79da7871d5c7e190c9282dd73c65fe632df
Dawid Cech <dawid.cech@gmail.com>
2020-03-18 20:28:47 +0100

    card refactoring

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect, ChangeEvent } from 'react'
-import { Card, Rate, Spin, Empty } from 'antd'
-import Meta from 'antd/lib/card/Meta'
+import { Empty, Spin } from 'antd'
+// import Meta from 'antd/lib/card/Meta'
 import { useHistory, useLocation } from 'react-router-dom'
-import { TextField, InputAdornment } from '@material-ui/core'
+import {
+  Card,
+  TextField,
+  InputAdornment,
+  makeStyles,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core'
+import Rating from '@material-ui/lab/Rating'
 import SearchIcon from '@material-ui/icons/Search'
 
 type Business = {
@@ -12,9 +24,19 @@ type Business = {
   price: string
   phone: string
   rating: number
+  review_count: number
 }
 
 const useQuery = () => new URLSearchParams(useLocation().search)
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+})
 
 export const SearchPage = () => {
   const [businesses, setBusinesses] = useState<Business[]>([])
@@ -22,6 +44,7 @@ export const SearchPage = () => {
   const query = useQuery().get('query')
   const [inputValue, setInputValue] = useState<string>(query!)
   const history = useHistory()
+  const classes = useStyles()
 
   useEffect(() => {
     setLoading(true)
@@ -44,7 +67,6 @@ export const SearchPage = () => {
             history.push(`/?query=${inputValue}`)
           }
         }}
-        defaultValue={query}
         value={inputValue}
         InputProps={{
           endAdornment: (
@@ -57,21 +79,49 @@ export const SearchPage = () => {
         style={{ marginTop: 20 }}
       />
       <Spin spinning={loading}>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: 10 }}>
-          {businesses.map((b: Business) => (
-            <Card
-              key={b.id}
-              style={{ margin: 10, width: 300 }}
-              cover={<img alt='example' src={b.image_url} />}
-              onClick={e => history.push(`/business/${b.id}`)}
-              hoverable
-            >
-              <Meta title={b.name} description={`price: ${b.price} phone: ${b.phone}`} />
-              <Rate disabled allowHalf defaultValue={b.rating}></Rate>
-            </Card>
-          ))}
-          {businesses.length === 0 && !loading && <Empty />}
-        </div>
+        {/* <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: 10 }}> */}
+        {businesses.map((b: Business) => (
+          // <Card
+          //   key={b.id}
+          //   style={{ margin: 10, width: 300 }}
+          //   cover={<img alt='example' src={b.image_url} />}
+          //   onClick={e => history.push(`/business/${b.id}`)}
+          //   hoverable
+          // >
+          //   <Meta title={b.name} description={`price: ${b.price} phone: ${b.phone}`} />
+          //   <Rate disabled allowHalf defaultValue={b.rating}></Rate>
+          // </Card>
+          <Card className={classes.root} key={b.id}>
+            <CardActionArea onClick={e => history.push(`/business/${b.id}`)}>
+              <CardMedia className={classes.media} image={b.image_url} title={b.name} />
+              <CardContent>
+                <Typography gutterBottom variant='h5' component='h2'>
+                  {b.name}
+                </Typography>
+                <Typography variant='body2' color='textSecondary' component='p'>
+                  price: {b.price}
+                  phone: {b.phone}
+                </Typography>
+                <Rating defaultValue={b.rating} precision={0.1} readOnly></Rating>
+                {b.review_count > 0 && (
+                  <Typography variant='body2' color='textSecondary' component='p'>
+                    {b.review_count} {b.review_count === 1 ? 'review' : 'reviews'}
+                  </Typography>
+                )}
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size='small' color='primary'>
+                Share
+              </Button>
+              <Button size='small' color='primary'>
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+        {businesses.length === 0 && !loading && <Empty />}
+        {/* </div> */}
       </Spin>
     </>
   )
