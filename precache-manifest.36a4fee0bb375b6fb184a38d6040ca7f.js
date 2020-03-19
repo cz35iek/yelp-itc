3c1c5d6e4ccc808b74e59a298298d4469110daac
Dawid Cech <dawid.cech@gmial.com>
2020-03-19 07:09:37 +0100

    Updates

---
+++ precache-manifest.36a4fee0bb375b6fb184a38d6040ca7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68172a172a64657981c5195bd1416e2",
+    "url": "/yelp-itc/index.html"
+  },
+  {
+    "revision": "ad982d07f4683ca56b88",
+    "url": "/yelp-itc/static/css/main.552b72a3.chunk.css"
+  },
+  {
+    "revision": "a92d51c6fb4e5c92893e",
+    "url": "/yelp-itc/static/js/2.2b67790c.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/yelp-itc/static/js/2.2b67790c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad982d07f4683ca56b88",
+    "url": "/yelp-itc/static/js/main.21697efb.chunk.js"
+  },
+  {
+    "revision": "607c625d5f67bea26ea7",
+    "url": "/yelp-itc/static/js/runtime-main.a2b349ed.js"
+  }
+]);
